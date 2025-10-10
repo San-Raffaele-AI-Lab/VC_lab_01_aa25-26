ce526ab5e0780cd7f17843f8b40ba6ced419956e
Thomas A. Ciarfuglia <ciarfuglia@diag.uniroma1.it>
2025-10-10 18:17:27 +0200

    Added (old style) solutions

--- src/access_image.cpp
+++ src/access_image.cpp
@@ -10,10 +10,10 @@
 // returns the 0-based location of the pixel value in the data array
 int pixel_address(const Image &im, int x, int y, int ch) {
 
-    // TODO: calculate and return the index
-
-    NOT_IMPLEMENTED();
-    return 0;
+    int pixel_idx = 0;
+    int ch_size = im.w * im.h;
+    pixel_idx = (ch * ch_size) + (y * im.w) + x;
+    return pixel_idx;
 }
 
 // HW0 #1
@@ -21,19 +21,25 @@ int pixel_address(const Image &im, int x, int y, int ch) {
 // int x,y,ch: pixel coordinates and channel of interest
 // returns the value of the clamped pixel at channel ch
 float get_clamped_pixel(const Image &im, int x, int y, int ch) {
-
     // TODO: clamp the coordinates and return the correct pixel value
 
-    NOT_IMPLEMENTED();
-    return 0.0;
+    if (x < 0) x = 0;
+    if (x >= im.w) x = im.w - 1;
+    if (y < 0) y = 0;
+    if (y >= im.h) y = im.h - 1;
+    if (ch < 0) ch = 0;
+    if (ch >= im.c) ch = im.c - 1;
+
+    return im.data[pixel_address(im, x, y, ch)];
+    //NOT_IMPLEMENTED();
+    //return 0.0;
 }
 
-
+// TODO: modify for unexpected values
 // HW0 #1
 // Image& im: input image
 // int x,y,ch: pixel coordinates and channel of interest
 void set_pixel(Image &im, int x, int y, int c, float value) {
-    // TODO: Only set the pixel to the value if it's inside the image
-
-    NOT_IMPLEMENTED();
+    if ((x >= 0 && x < im.w) && (y >= 0 && y < im.h) && (c >= 0 && c < im.c))
+        im.data[pixel_address(im, x, y, c)] = value;
 }
--- src/process_image.cpp
+++ src/process_image.cpp
@@ -10,14 +10,18 @@ using namespace std;
 // HW0 #3
 // const Image& im: input image
 // return the corresponding grayscale image
+// HW0 #3.1 optimize the code
 Image rgb_to_grayscale(const Image &im) {
     assert(im.c == 3); // only accept RGB images
     Image gray(im.w, im.h, 1); // create a new grayscale image (note: 1 channel)
 
-    // TODO: calculate the pixels of 'gray'
+    // TODO: optimize
+    for (int i = 0; i < im.w; ++i) {
+        for (int j = 0; j < im.h; ++j) {
+            gray(i, j, 0) = 0.299 * im(i, j, 0) + 0.587 * im(i, j, 1) + 0.114 * im(i, j, 2);
+        }
 
-
-    NOT_IMPLEMENTED();
+    }
 
     return gray;
 }
@@ -47,8 +51,12 @@ void shift_image(Image &im, int c, float v) {
     assert(c >= 0 && c < im.c); // needs to be a valid channel
 
     // TODO: shift all the pixels at the specified channel
-
-    NOT_IMPLEMENTED();
+    int ch_size = im.w*im.h;
+    for (int i = 0; i < im.w; ++i) {
+        for (int j = 0; j < im.h; ++j) {
+            im(i,j,c) += v;
+        }
+    }
 
 
 }
@@ -61,8 +69,12 @@ void scale_image(Image &im, int c, float v) {
     assert(c >= 0 && c < im.c); // needs to be a valid channel
 
     // TODO: scale all the pixels at the specified channel
-
-    NOT_IMPLEMENTED();
+    int ch_size = im.w*im.h;
+    for (int i = 0; i < im.w; ++i) {
+        for (int j = 0; j < im.h; ++j) {
+            im(i,j,c) *= v;
+        }
+    }
 
 }
 
@@ -71,8 +83,18 @@ void scale_image(Image &im, int c, float v) {
 // Image& im: input image to be modified in-place
 void clamp_image(Image &im) {
     // TODO: clamp all the pixels in all channel to be between 0 and 1
-
-    NOT_IMPLEMENTED();
+    for (int k = 0; k < im.c; ++k) {
+        for (int i = 0; i < im.w; ++i) {
+            for (int j = 0; j < im.h; ++j) {
+                if (im(i,j,k) > 1) {
+                    im(i,j,k)=1;
+                }
+                if (im(i,j,k)<0){
+                    im(i,j,k)=0;
+                }
+            }
+        }
+    }
 
 
 }
@@ -93,8 +115,55 @@ void rgb_to_hsv(Image &im) {
     assert(im.c == 3 && "only works for 3-channels images");
 
     // TODO: Convert all pixels from RGB format to HSV format
-
-    NOT_IMPLEMENTED();
+    float V, S, H;
+    float m, C;
+    float RGB[3];
+    float &R = RGB[0];
+    float &G = RGB[1];
+    float &B = RGB[2];
+    int value_idx;
+    for (int i = 0; i < im.w; ++i) {
+        for (int j = 0; j < im.h; ++j) {
+            RGB[0] = im(i,j,0);
+            RGB[1] = im(i,j,1);
+            RGB[2] = im(i,j,2);
+            // Value
+            value_idx = max_element(RGB, RGB+3) - RGB;
+            V = RGB[value_idx];
+            // Saturation
+            m = *min_element(RGB, RGB+3);
+            C = V - m;
+            if (V==0){
+                S = 0;
+            }
+            else {
+                S = C / V;
+            }
+            // Hue
+            if (C == 0) {
+                H = 0;
+            }
+            else {
+                switch (value_idx) {
+                    case 0:
+                        H = (G-B)/C;
+                        break;
+                    case 1:
+                        H = (B-R)/C+2;
+                        break;
+                    case 2:
+                        H = (R-G)/C+4;
+                        break;
+                }
+            }
+            H = H/6;
+            if (H<0) H++;
+            // write the new color space values to im
+            im(i,j,0)=H;
+            im(i,j,1)=S;
+            im(i,j,2)=V;
+        }
+    }
 
 }
 
@@ -104,8 +173,56 @@ void hsv_to_rgb(Image &im) {
     assert(im.c == 3 && "only works for 3-channels images");
 
     // TODO: Convert all pixels from HSV format to RGB format
-
-    NOT_IMPLEMENTED();
+    float H, S, V;
+    float C, X, m;
+    float R, G, B;
+    for (int i = 0; i < im.w; ++i) {
+        for (int j = 0; j < im.h; ++j) {
+            H = im(i,j,0);
+            S = im(i,j,1);
+            V = im(i,j,2);
+            C = V * S;
+            X = C * (1-abs(fmod(6*H, 2) - 1));
+            m = V - C;
+            if (H<1.0/6) {
+                R = C;
+                G = X;
+                B = 0;
+            }
+            else if(H<2.0/6) {
+                R = X;
+                G = C;
+                B = 0;
+            }
+            else if (H<3.0/6) {
+                R = 0;
+                G = C;
+                B = X;
+            }
+            else if (H<4.0/6) {
+                R = 0;
+                G = X;
+                B = C;
+            }
+            else if (H<5.0/6) {
+                R = X;
+                G = 0;
+                B = C;
+            }
+            else {
+                R = C;
+                G = 0;
+                B = X;
+            }
+            R+=m;
+            G+=m;
+            B+=m;
+            // update image values
+            im(i,j,0) = R;
+            im(i,j,1) = G;
+            im(i,j,2) = B;
+        }
+    }
 
 
 }
